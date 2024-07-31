a77a939d0f7d6e492a8eaf723259f715c8316a95
prestonm44 <preston.mitchell4444@gmail.com>
2024-07-30 21:32:20 -0600

    adding api routes

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ import morgan from 'morgan';
 import ViteExpress from 'vite-express';
 
 import appRouter from './routes/index.js';
+import { Building } from './models/model.js';
 
 const app = express();
 const port = '8000';
@@ -13,8 +14,23 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(session({ secret: 'ssshhhhh', saveUninitialized: true, resave: false }));
-
 app.use(appRouter);
+
+app.get('/api/buildings', async (req, res) => {
+    Building.findAll().then((buildingData) => {
+        res.json(buildingData)
+    }
+    );
+});
+
+app.get('/api/users', async(re))
+
+
+
+
+
+
+
 
 
 ViteExpress.listen(app, port, () => console.log(`Server is listening on http://localhost:${port}`));
